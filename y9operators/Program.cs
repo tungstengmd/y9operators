fd6f497f13f82e13cb1d41809a5a0637e604f7f4
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-12-18 12:03:40 +0000

    make stuff :3

--- y9operators/Program.cs
+++ y9operators/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace y9operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            if (x < y) {
+                Console.WriteLine("hello :3");
+            }
+            else {
+                Console.WriteLine("world (bug bug)");
+            }
+        }
+    }
+}
