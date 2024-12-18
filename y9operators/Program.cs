5b6abeb50895231efd32af1fe1c1bcea9ac1f6b6
Harlow 3 <22BKarolia@heckgrammar.co.uk>
2024-12-18 12:09:54 +0000

    tabs

--- y9operators/Program.cs
+++ y9operators/Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace y9operators
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 20;
-            int y = 19;
-            int z = 3;
-            if (x <= y || z > y) {
-                Console.WriteLine("hello :3");
-            }
-            else {
-                Console.WriteLine("world (bug bug)");
-            }
-        }
-    }
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 20;
+			int y = 19;
+			int z = 3;
+			if (x <= y || z > y) {
+				Console.WriteLine("hello :3");
+			}
+			else {
+				Console.WriteLine("world (bug bug)");
+			}
+		}
+	}
 }
